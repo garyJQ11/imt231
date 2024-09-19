0eb56f87cb17828715ddecd679913e4e361d9198
gary <gari.jimmi@gmail.com>
2024-09-19 11:22:08 -0400

    2024-09-19_01 controla si es =,>,<

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -6,6 +6,20 @@ cout << "* Bienvenido al juego de la adivinanza! *"<<endl;
 cout << "*****************************************"<<endl;
 
 int numero_secreto=42;
-cout<< "el numero secreto es . ."<< numero_secreto << " no se lo diga nadie"<<endl;
+cout<< "el numero secreto es ..."<< numero_secreto << ". no se lo diga a nadie"<<endl;
+
+int adivina;
+cout<<"cual es el numero?";
+cin>>adivina;
+cout<<"Su numero ingresado es: "<<adivina<<endl;
+if(adivina==numero_secreto){
+cout<<"Correcto, .. adivino el numero secreto"<<endl;
+}
+else if(adivina>numero_secreto){
+cout<<"El numero ingresado es mayor al numero secreto"<<endl;
+}
+else if(adivina<numero_secreto){
+cout<<"El numero ingresado es menor al  numero secreto"<<endl;
+}
 
 }
