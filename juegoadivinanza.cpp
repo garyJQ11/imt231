b5bfeaa9b1257b3c162cfe892ce2b55fc577f431
gary <gari.jimmi@gmail.com>
2024-09-19 10:49:38 -0400

    2024-09-19_01 iniciando código juego de la adivinanza en c++

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "*****************************************"<<endl;
+cout << "* Bienvenido al juego de la adivinanza! *"<<endl;
+cout << "*****************************************"<<endl;
+
+int numero_secreto=42;
+cout<< "el numero secreto es . ."<< numero_secreto << " no se lo diga nadie"<<endl;
+
+}
